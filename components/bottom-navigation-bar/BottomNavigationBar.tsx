bf41349e9c03be7d38755ecdf0a122a5470c7162
laser-ayyildiz <laserayyildiz@gmail.com>
2022-10-28 23:09:58 +0300

    navbar icons have been added

--- components/bottom-navigation-bar/BottomNavigationBar.tsx
+++ components/bottom-navigation-bar/BottomNavigationBar.tsx
@@ -1,18 +1,33 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Budget from '../budget/Budget';
 import Home from '../home/Home';
 import Hydrate from '../hydrate/Hydrate';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
 const BottomNavigationBar = () => {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Hydrate" component={Hydrate} />
-      <Tab.Screen name="Budget" component={Budget} />
-    </Tab.Navigator>
-  )
+    return (
+        <Tab.Navigator screenOptions={({route}) => ({
+            tabBarIcon: ({focused, color, size}) => {
+                let iconName;
+                if (route.name === 'Hydrate') {
+                    iconName = focused ? 'water' : 'water-outline';
+                } else if (route.name === 'Home') {
+                    iconName = focused ? 'home' : 'home-outline';
+                } else if (route.name === 'Budget') {
+                    iconName = focused ? 'wallet' : 'wallet-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+            },
+            tabBarActiveTintColor: 'black',
+            tabBarInactiveTintColor: 'gray',
+        })}>
+            <Tab.Screen name="Home" component={Home}/>
+            <Tab.Screen name="Hydrate" component={Hydrate}/>
+            <Tab.Screen name="Budget" component={Budget}/>
+        </Tab.Navigator>
+    )
 }
 
 export default BottomNavigationBar
